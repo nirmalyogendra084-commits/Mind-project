4853232c319daa955da291f04e75e1f77b41561c
yogee <nirmalyogendra084@gmail.com>
2026-03-13 08:12:51 +0000

    update project

---
+++ exports/root/src/react-app/App.tsx
@@ -0,0 +1,70 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Layout from "@/react-app/components/Layout";
+import Dashboard from "@/react-app/pages/Dashboard";
+import Analytics from "@/react-app/pages/Analytics";
+import Profile from "@/react-app/pages/Profile";
+
+export default function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+jj;
+git add .
+git commit -m "update project"
+
+date project"
+
+update project"
+
+date project"
+
+ "update project"
+
+date project"
+
+update project"
+
+date project"
+
+
+ect"
+
+update project"
+
+date project"
+
+
+roject"
+
+
+oject"
+
+date project"
+
+
+roject"
+
+
+oject"
+
+ct"
+
+
+roject"
+
+
+oject"
+
+
+
+
+oject"
